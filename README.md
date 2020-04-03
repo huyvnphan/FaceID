90c56ff0782d74fc1a3d2e00f4fd4d0d20501b4b
Huy Phan <huyvnphan@gmail.com>
2020-04-03 15:34:46 -0400

    Update hyper parameters

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# faceid
+# FaceID in PyTorch
+Inspired by this blog https://towardsdatascience.com/how-i-implemented-iphone-xs-faceid-using-deep-learning-in-python-d5dbaa128e1d
+I decided to reimplement the FaceID authentication in PyTorch
--- faceid_system.py
+++ faceid_system.py
@@ -11,12 +11,16 @@ class FaceIDSystem(pl.LightningModule):
         self.model = SqueezeNet()
         self.loss = torch.nn.CosineEmbeddingLoss(margin=0.5)
         self.cosine = torch.nn.CosineSimilarity()
+        self.threshold = 0.8
         
-    def calculate_cosine(self, embed_x0, embed_x1, y):
+    def calculate_stat(self, embed_x0, embed_x1, y):
         cosine = self.cosine(embed_x0, embed_x1)
-        same_class_cosine = cosine[y==1].mean()
-        diff_class_cosine = cosine[y==-1].mean()
-        return same_class_cosine, diff_class_cosine
+        same_class_cosine = cosine[y==1]
+        diff_class_cosine = cosine[y==-1]
+        true_positive = (same_class_cosine > self.threshold).sum()
+        true_negative = (diff_class_cosine < self.threshold).sum()
+        accuracy = (true_positive + true_negative) / float(y.size(0))
+        return same_class_cosine.mean(), diff_class_cosine.mean(), accuracy
                     
     def forward(self, x0, x1):
         embed_x0 = self.model(x0)
@@ -27,33 +31,36 @@ class FaceIDSystem(pl.LightningModule):
         x0, x1, y = batch
         embed_x0, embed_x1 = self.forward(x0, x1)
         loss = self.loss(embed_x0, embed_x1, y) 
-        logs = {'train_loss': loss}
+        logs = {'loss/train': loss}
         return {'loss': loss, 'log': logs}
         
     def validation_step(self, batch, batch_nb):
         x0, x1, y = batch
         embed_x0, embed_x1 = self.forward(x0, x1)
         loss = self.loss(embed_x0, embed_x1, y)
-        same_class_cosine, diff_class_cosine = self.calculate_cosine(embed_x0, embed_x1, y)
-        logs = {'val_loss': loss,
-                'same_class_cosine': same_class_cosine,
-                'diff_class_cosine': diff_class_cosine}
+        same_class_cosine, diff_class_cosine, accuracy = self.calculate_stat(embed_x0, embed_x1, y)
+        logs = {'loss/val': loss,
+                'cosine/same_class': same_class_cosine,
+                'cosine/diff_class': diff_class_cosine,
+                'accuracy': accuracy}
         return logs
                 
     def validation_epoch_end(self, outputs):
-        avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
-        avg_same_class_cosine = torch.stack([x['same_class_cosine'] for x in outputs]).mean()
-        avg_diff_class_cosine = torch.stack([x['diff_class_cosine'] for x in outputs]).mean()
+        loss = torch.stack([x['loss/val'] for x in outputs]).mean()
+        same_class_cosine = torch.stack([x['cosine/same_class'] for x in outputs]).mean()
+        diff_class_cosine = torch.stack([x['cosine/diff_class'] for x in outputs]).mean()
+        accuracy = torch.stack([x['accuracy'] for x in outputs]).mean()
         
-        logs = {'val_loss': avg_loss,
-                'same_class_cosine': avg_same_class_cosine,
-                'diff_class_cosine': avg_diff_class_cosine}
-        return {'val_loss': avg_loss, 'log': logs}
+        logs = {'loss/val': loss,
+                'cosine/same_class': same_class_cosine,
+                'cosine/diff_class': diff_class_cosine,
+                'accuracy': accuracy}
+        return {'val_loss': loss, 'log': logs}    
     
     def configure_optimizers(self):
-        optimizer = torch.optim.AdamW(self.model.parameters(), lr=1e-3, weight_decay=1e-2)
+        optimizer = torch.optim.AdamW(self.model.parameters(), lr=1e-4, weight_decay=1e-3)
         scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=50, gamma=0.1)
-        return [optimizer], [scheduler]
+        return optimizer
     
     def train_dataloader(self):
         return get_dataloader(self.hparams.data_dir, self.hparams.batch_size)
