2f8254d6c3cb54df315e3e341f0a8b26e5fae358
Huy Phan <huyvnphan@gmail.com>
2020-04-03 18:55:41 -0400

    Update README.md

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # FaceID in PyTorch
 Inspired by this blog https://towardsdatascience.com/how-i-implemented-iphone-xs-faceid-using-deep-learning-in-python-d5dbaa128e1d
+
 I decided to reimplement the FaceID authentication in PyTorch
+
+# Requirements
+tqdm >= 4.44.1
+numpy >= 1.18.1
+pytorch >= 1.4.0
+torchvision > 0.5.0
+tensorboard >= 2.20
+pytorch-lightning >= 0.7.1
--- faceid_main.py
+++ faceid_main.py
@@ -13,15 +13,16 @@ def main(hparams):
     torch.manual_seed(0)
     
     model = FaceIDSystem(hparams)
-    trainer = Trainer(gpus=[hparams.gpus], min_epoch=hparams.min_epoch)
+    trainer = Trainer(gpus=[hparams.gpus], early_stop_callback=False, max_epoch=hparams.epoch)
     trainer.fit(model)
 
 if __name__ == '__main__':
     parser = ArgumentParser()
+    parser.add_argument('--description', type=str, default='Default')
     parser.add_argument('--data_dir', type=str, default='/raid/data/pytorch_dataset/faceid/')
     parser.add_argument('--gpus', type=int, default=1)
     parser.add_argument('--batch_size', type=int, default=64)
-    parser.add_argument('--min_epoch', type=int, default=150)
+    parser.add_argument('--epoch', type=int, default=200)
     args = parser.parse_args()
 
     main(args)
