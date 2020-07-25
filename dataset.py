6bc8823be133ace7c86eff430f630c5800ca6c3a
Huy Phan <huykid1206@gmail.com>
2020-07-25 19:16:27 -0400

    More augmentation

--- dataset.py
+++ dataset.py
@@ -27,9 +27,8 @@ class FaceIDDataset(Dataset):
             self.no_people = 26
             self.transform = transforms.Compose(
                 [
-                    transforms.CenterCrop(300),
-                    transforms.RandomCrop(256),
                     transforms.RandomHorizontalFlip(),
+                    transforms.RandomResizedCrop(256, scale=(0.25, 1.0)),
                     transforms.ToTensor(),
                     transforms.Normalize(mean, std),
                 ]
